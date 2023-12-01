01c44c979bc8c6c161cf6d5a9b280cd76a7ec7fc
KarrieB26 <152208019+KarrieB26@users.noreply.github.com>
2023-12-01 11:10:21 -0600

    Update main.cpp
    Adding the base code for the lab

--- main.cpp
+++ main.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
 using namespace std;
+#include "PlaylistNode.h"
+
+using namespace std;
+
+void PrintMenu(const string playlistTitle) {
+   /* Type your code here */
+   
+}
+
+PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
+   /* Type your code here */
+   
+}
+
+int main() {
+   /* Type your code here */
+   
+   return 0;
+}
+
